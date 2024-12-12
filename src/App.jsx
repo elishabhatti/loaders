cb782427b36d38fb33d587156c74853cccad6fc0
elisha <elishabhatti2007@gmail.com>
2024-12-12 14:05:19 +0500

    added more loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function App() {
   return (
-    <div className="flex justify-center items-center flex-col h-screen">
+    <div className="flex justify-around bg-[#131B2E]  items-center h-screen">
       <button
         type="button"
         className="bg-indigo-500 text-white flex justify-center items-center py-2 px-3 text-lg  rounded-md"
@@ -29,6 +29,27 @@ export default function App() {
           ></path>
         </svg>
         Processing...
+      </button>
+      <button>
+        <div class="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
+          <svg
+            class="w-6 h-6 text-violet-500"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+          </svg>
+        </div>
+      </button>
+      <button>
+        <span class="relative flex h-6 w-6">
+          <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+          <span class="relative inline-flex rounded-full h-6 w-6 bg-sky-500"></span>
+        </span>
       </button>
     </div>
   );
